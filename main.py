60c8af6400276a2ddcf6899b4fe8698eea63e089
Prajwal Rao <prajwaljpj@gmail.com>
2019-11-05 17:50:10 +0530

    integration of deepsort (under process)

--- main.py
+++ main.py
@@ -1,16 +1,94 @@
 import os
+import glob
 import sys
 import struct
-import subprocess
-from multiprocessing import Process
 import errno
 import cv2
-from python.boundbox import Box, wrap_box
+from python.boundbox import Box
+from python.super_frame import SuFrame
+from python.deepsort import deepsort_tracker
+import configparser
 
+class Analytics(object):
+    """Top level of the analytics pipeline
 
-def setBbox():
-    cmd = "/home/rbccps2080ti/projects/TensorRT-Yolov3/build/runYolov3"
-    subprocess.Popen(cmd)
+    """
+    def __init__(self, segment_source, pipe_path, line_coordinates):
+        super(Analytics, self).__init__()
+        self.segment_source = segment_source
+        self.pipe_path = pipe_path
+        self.line_coordinates = line_coordinates
+        
+        parser = configparser.SafeConfigParser()
+        parser.read(["../configs/global.cfg", "../configs/Global.cfg", "../configs/globals.cfg"])
+
+    def get_latest(self, segment_path):
+        list_of_files = glob.glob(os.path.join(segment_path, "*.flv")) # * means all if need specific format then *.csv
+        latest_file = max(list_of_files, key=os.path.getctime)
+        return latest_file
+
+    def get_video_to_process(self):
+        if not os.path.exists(self.segment_source):
+            os.mkdir(self.segment_source)
+        list_of_files = glob.glob(os.path.join(self.segment_source, "*.flv"))
+        sorted_list = sorted(list_of_files, key=os.path.getmtime)
+        if len(sorted_list) != 0:
+            latest_file = sorted_list[-1]
+            return latest_file
+        return []
+
+    def check_and_delete(self):
+        list_of_files = glob.glob(os.path.join(self.segment_source, "*.flv"))
+        if len(list_of_files) > 10:
+            sorted_list = sorted(list_of_files, key=os.path.getmtime)
+            for segfile in sorted_list[:len(sorted_list)-2]:
+                os.remove(segfile)
+
+    def getboxval(self):
+        try:
+            os.mkfifo(self.pipe_path)
+        except OSError as oe:
+            if oe.errno != errno.EEXIST:
+                raise
+        fifo = os.open(self.pipe_path, os.O_RDONLY)
+        return fifo
+
+    def run_analytics(self, fifo):
+        DeepSort = deepsort_tracker()
+        while(True):
+            frame_number = 1
+            latest_video = self.get_video_to_process()
+            self.check_and_delete()
+            fifo_pipe = self.getboxval()
+            cap = cv2.VideoCapture(latest_video)
+            while (cap.isOpened()):
+                ret, frame = cap.read()
+                if not ret:
+                    break
+
+                sframe = SuFrame(frame)
+
+                try:
+                    head = os.read(fifo, 1)
+                except:
+                    print("didnt get header for frame")
+                    continue
+                head = int.from_bytes(head, "big")
+                frame_number += 1
+                if head == 0:
+                    continue
+
+                detections = []
+
+                for i in range(head):
+                    data_bytes = os.read(fifo, 24)
+                    data = struct.unpack("=iiiiif", data_bytes)
+                    detections.append(Box(data))
+                sframe.set_dets(detections)
+                # yolo is done above
+                # deepsort
+                sframe = deepsort_tracker.run_deep_sort(sFrame)
+
 
 
 def getBbox():
@@ -27,7 +105,7 @@ def getBbox():
     frame_number = 1
 
     while(True):
-        videocap = cv2.VideoCapture("latest.mp4")
+        
         if (videocap.isOpened() == False):
             print("python side ::::::::: Error opening video stream or file")
         out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 20, (1280, 720))
@@ -69,12 +147,4 @@ def getBbox():
 
 
 if __name__ == "__main__":
-
-    p1 = Process(target=setBbox)
-    p2 = Process(target=getBbox)
-
-    p1.start()
-    p2.start()
-
-    p1.join()
-    p2.join()
+    getBbox()
--- python/line_counts.py
+++ python/line_counts.py
@@ -10,11 +10,13 @@ class counts(object):
         super(counts, self).__init__()
         self.line_coord = line_coord
         parser = configparser.SafeConfigParser()
-        found = parser.read(["../configs/global.cfg", "../configs/Global.cfg", "../configs/globals.cfg"])
+        _ = parser.read(["../configs/global.cfg", "../configs/Global.cfg", "../configs/globals.cfg"])
         self.yolo_model = parser.get("Yolo", "model_path")
 
 
     def get_count(self, super_frame, time_slot):
         frame = super_frame.get_image()
         dets = super_frame.get_dets()
-        trackers = deepsort_tracker.run_deep_sort(frame, super_frame.get_scores(), super_frame.get_ds_boxes(), super_frame.get_class_names())
+        deepsort = deepsort_tracker()
+        trackers = deepsort.run_deep_sort(frame, super_frame.get_scores(), super_frame.get_ds_boxes(), super_frame.get_class_names())
+        print(trackers)
